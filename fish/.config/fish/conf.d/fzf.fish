feede9acd9d8033540ee73b10e600f0f6abf69fe
ardisprime <christoffer.marschner@online.de>
2025-01-06 21:03:13 +0100

    added fzf config

---
+++ fish/.config/fish/conf.d/fzf.fish
@@ -0,0 +1,4 @@
+
+# set default fzf options
+set -gx FZF_DEFAULT_OPTS "--color='fg:#bbbbff,hl:#bbbbff,fg+:#00001a,bg+:#bbbbff,hl+:#00001a,border:#bbbbff,prompt:#bbbbff' --marker='' --pointer='' --no-info --no-separator --no-scrollbar --border=rounded --tabstop=2 --height=75% --layout=reverse --margin=1,1"
+
