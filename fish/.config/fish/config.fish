7140bcae71551e0847cc070f7eeb82f0013168c0
ardisprime <christoffer.marschner@online.de>
2025-02-05 10:31:15 +0100

    added fcitx5 autostart

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,10 @@
 
 # autostart niri if in tty
 if test $TERM = linux
-  niri --session
+  set -gx GTK_IM_MODULE fcitx
+  set -gx QT_IM_MODULE fcitx
+  set -gx XMODIFIERS @im=fcitx
+  dbus-run-session niri --session
 end
 
 # set fish greeting
