43c9a5fb8ac9a95258cfc21a5fc821f70fb890da
ardisprime <christoffer.marschner@online.de>
2025-03-03 14:45:52 +0100

    added fzf color config

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -1,4 +1,4 @@
 
 # set default fzf options
-set -gx FZF_DEFAULT_OPTS "--color='fg:#bbbbff,hl:#bbbbff,fg+:#00001a,bg+:#bbbbff,hl+:#00001a,border:#bbbbff,prompt:#bbbbff' --marker='' --pointer='' --no-info --no-separator --no-scrollbar --border=rounded --tabstop=2 --height=75% --layout=reverse --margin=1,1"
+set -gx FZF_DEFAULT_OPTS "--color='fg:#934B14,hl:#934B14,fg+:#081021,bg+:#934B14,hl+:#081021,border:#934B14,prompt:#934B14' --marker='' --pointer='' --no-info --no-separator --no-scrollbar --border=rounded --tabstop=2 --height=75% --layout=reverse --margin=1,1"
 
