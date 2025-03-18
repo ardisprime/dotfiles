5d098351161c3c3a3575e2e2a4f4608cf7a6bd4b
ardisprime <christoffer.marschner@online.de>
2025-03-18 08:33:18 +0100

    added color change for fcitx

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -1,4 +1,4 @@
 
 # set default fzf options
-set -gx FZF_DEFAULT_OPTS "--color='fg:#934B14,hl:#934B14,fg+:#081021,bg+:#934B14,hl+:#081021,border:#934B14,prompt:#934B14' --marker='' --pointer='' --no-info --no-separator --no-scrollbar --border=rounded --tabstop=2 --height=75% --layout=reverse --margin=1,1"
+set -gx FZF_DEFAULT_OPTS "--color='fg:#8696A4,hl:#8696A4,fg+:#03060F,bg+:#8696A4,hl+:#03060F,border:#8696A4,prompt:#8696A4' --marker='' --pointer='' --no-info --no-separator --no-scrollbar --border=rounded --tabstop=2 --height=75% --layout=reverse --margin=1,1"
 
