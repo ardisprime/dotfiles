8f7d9f6dc5745c6c425e8a1768dcdf16a63f3d4f
ardisprime <christoffer.marschner@online.de>
2025-04-27 18:52:07 +0200

    [niri] o changed sound shortcuts to proper ones

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -1,4 +1,4 @@
 
 # set default fzf options
-set -gx FZF_DEFAULT_OPTS "--color='fg:#8696A4,hl:#8696A4,fg+:#03060F,bg+:#8696A4,hl+:#03060F,border:#8696A4,prompt:#8696A4' --marker='' --pointer='' --no-info --no-separator --no-scrollbar --border=rounded --tabstop=2 --height=75% --layout=reverse --margin=1,1"
+set -gx FZF_DEFAULT_OPTS "--color='fg:#C6CCC5,hl:#C6CCC5,fg+:#01031C,bg+:#C6CCC5,hl+:#01031C,border:#C6CCC5,prompt:#C6CCC5' --marker='' --pointer='' --no-info --no-separator --no-scrollbar --border=rounded --tabstop=2 --height=75% --layout=reverse --margin=1,1"
 
