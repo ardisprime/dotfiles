d1740b716571b2e646a86a381ce054bd9f7b9e35
ardisprime <christoffer.marschner@online.de>
2025-04-27 13:35:09 +0200

    changed naming for package manager wrapper functions

--- fish/.config/fish/functions/package_manager_wrapper.fish
+++ fish/.config/fish/functions/package_manager_wrapper.fish
@@ -2,17 +2,20 @@
 function package_manager_wrapper
 
   # parse the arguments
-  argparse -x i,u,r i/install u/update r/remove -- $argv
+  argparse -x i,u,r,s i/install u/update r/remove s/search -- $argv
 
   # set the corresponding cpmmand to run
   if set -q _flag_install 
-    set -g command_to_run install_os
+    set -g command_to_run pm_install_os
 
   else if set -q _flag_update
-    set -g command_to_run update_os
+    set -g command_to_run pm_update_os
 
   else if set -q _flag_remove
-    set -g command_to_run remove_os
+    set -g command_to_run pm_remove_os
+
+  else if set -q _flag_search
+    set -g command_to_run pm_search_os
 
   else
     echo "no flag given!"
