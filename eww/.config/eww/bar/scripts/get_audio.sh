2f71f9004600d0f536777ffde1ec754c83fcfed7
ardisprime <christoffer.marschner@online.de>
2025-05-12 15:00:30 +0200

    added an eww bar widget

---
+++ eww/.config/eww/bar/scripts/get_audio.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+pulseaudio_json="$(pactl -fjson list sinks)"
+selected_sink="$(pactl get-default-sink)"
+
+volume=0
+
+for sink in "$(echo $pulseaudio_json | jq -c '.[]' )"
+do
+  if test "$(echo $sink | jq -c .name)" = "\"$selected_sink\""
+  then
+    volume="$(echo $sink | jq -c '.volume["front-left"].value_percent')"
+    muted="$(echo $sink | jq -c '.mute')"
+  fi
+done
+
+echo "{\"volume\": $volume, \"muted\": $muted}"
+
