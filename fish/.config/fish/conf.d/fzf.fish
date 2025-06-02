da295fa91ab0a565cd4e3bf45b18133b679b44bf
ardisprime <christoffer.marschner@online.de>
2025-06-02 11:34:38 +0200

    [all] o changed color changer [ghostty] o decreased font size

--- fish/.config/fish/conf.d/fzf.fish
+++ fish/.config/fish/conf.d/fzf.fish
@@ -1,4 +1,14 @@
 
+# _{set0
+set color_dark 01121B
+# _}end
+# _{set3
+set color_light 55963C
+# _}end
+# _{set4
+set color_super_light AE9464
+# _}end
+
 # set default fzf options
-set -gx FZF_DEFAULT_OPTS "--color='fg:#D13712,hl:#D13712,fg+:#040804,bg+:#D13712,hl+:#040804,border:#D13712,prompt:#D13712' --marker='' --pointer='' --no-info --no-separator --no-scrollbar --border=rounded --tabstop=2 --height=75% --layout=reverse --margin=1,1"
+set -gx FZF_DEFAULT_OPTS "--color='fg:#$color_light,hl:#$color_light,fg+:#$color_super_light,bg+:#$color_dark,hl+:#$color_super_light,border:#$color_light,prompt:#$color_light' --marker='' --pointer='' --no-info --no-separator --no-scrollbar --border=rounded --tabstop=2 --height=75% --layout=reverse --margin=1,1"
 
